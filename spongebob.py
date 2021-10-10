561f80ba70b028e55a312d4a7c3a6cac2a402598
Sofi <sofi+git@mailbox.org>
2021-10-10 22:26:31 +0200

    Change to a proper functional approach

--- spongebob.py
+++ spongebob.py
@@ -18,19 +18,23 @@ Imperative: TeStInG! dOeSn'T tHiS lOoK gRe8T?
 1: https://knowyourmeme.com/memes/mocking-spongebob
 """
 from itertools import cycle
-from more_itertools import map_if
+from typing import Iterable
+
+from more_itertools import partition
 
 
-class Cycle:
-    def __init__(self, *functions):
-        self.iterable = cycle(functions)
-
-    def __call__(self, *args, **kwargs):
-        return next(self.iterable)(*args, **kwargs)
+def cycle_case(iterable: Iterable) -> Iterable:
+    return zip(cycle((str.upper, str.lower)), iterable)
 
 
 def spongebob_functional(text: str) -> str:
-    return "".join(map_if(text, pred=str.isalpha, func=Cycle(str.upper, str.lower)))
+    not_letters, letters = partition(str.isalpha, text)
+    cased_letters = (func(letter) for func, letter in cycle_case(letters))
+
+    return "".join(
+        next(cased_letters) if letter.isalpha() else next(not_letters)
+        for letter in text
+    )
 
 
 def spongebob_imperative(text: str) -> str:
@@ -49,6 +53,6 @@ def spongebob_imperative(text: str) -> str:
 
 
 if __name__ == "__main__":
-    example_text = "Testing! Doesn't this look gre8t?"
-    print("Functional:", spongebob_functional(example_text))
-    print("Imperative:", spongebob_imperative(example_text))
+    EXAMPLE_TEXT = "Testing! Doesn't this look gre8t?"
+    print("Functional:", spongebob_functional(EXAMPLE_TEXT))
+    print("Imperative:", spongebob_imperative(EXAMPLE_TEXT))
